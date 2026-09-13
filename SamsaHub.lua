4c1955b6ad0b41dbd672aba2152df13602eb530e
ExSamsa <leonmarketg@gmail.com>
2026-09-13 20:14:41 +0300

    Update SamsaHub.lua

--- SamsaHub.lua
+++ SamsaHub.lua
@@ -1,5 +1,4 @@
--- Загружаем интерфейс Rayfield
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu'))()
+local Rayfield = loadstring(game:HttpGet('https://githubusercontent.com'))()
 
 -- Инициализация окна
 local Window = Rayfield:CreateWindow({
